60e07ffb739d1cc620a18b3a855507c32c56df50
Nimesh8848 <nimeshmathema@gmail.com>
2024-11-27 13:13:28 +0545

    setup auto mapper

---
+++ HR.LeaveManagement.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using HR.LeaveManagement.Application.Profiles;
+
+namespace HR.LeaveManagement.Application
+{
+    public  static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection ConfigureApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            
+
+            return services;
+        }
+    }
+}
---
+++ HR.LeaveManagement.Application/DTOs/LeaveRequest/LeaveRequestListDto.cs
@@ -0,0 +1,16 @@
+﻿using HR.LeaveManagement.Application.DTOs.Common;
+using HR.LeaveManagement.Domain;
+using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HR.LeaveManagement.Application.DTOs.LeaveRequest
+{
+    public class LeaveRequestListDto: BaseDto
+    {
+        public LeaveTypeDto LeaveType { get; set; }
+        public DateTime DateRequested { get; set; }
+        public bool? Approved { get; set; }
+    }
+}
