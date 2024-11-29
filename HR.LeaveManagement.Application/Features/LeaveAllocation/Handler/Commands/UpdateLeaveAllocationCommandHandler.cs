8c0e39fb74433d86f32ee5665f8c74322c36e914
Nimesh8848 <nimeshmathema@gmail.com>
2024-11-29 11:35:56 +0545

    Update for all the feature is completed now the remainng is delete CQRS

--- HR.LeaveManagement.Application/Features/LeaveAllocation/Handler/Commands/UpdateLeaveAllocationCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Handler/Commands/UpdateLeaveAllocationCommandHandler.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Request.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Handler.Commands
 {
-    internal class UpdateLeaveAllocationCommandHandler
+    public class UpdateLeaveAllocationCommandHandler : IRequestHandler<UpdateLeaveAllocationCommand, Unit>
     {
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly Mapper _mapper;
+
+        public UpdateLeaveAllocationCommandHandler(ILeaveAllocationRepository leaveAllocationRepository,Mapper mapper) {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveAllocationCommand request, CancellationToken cancellationToken)
+        {
+            var Leaveallocation = await _leaveAllocationRepository.Get(request.LeaveAllocationDto.Id);
+             _mapper.Map(request.LeaveAllocationDto, Leaveallocation);
+            await _leaveAllocationRepository.Update(Leaveallocation);
+            return Unit.Value;
+        }
     }
 }
--- HR.LeaveManagement.Application/Features/LeaveRequest/Handler/Commands/UpdateLeaverequestCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveRequest/Handler/Commands/UpdateLeaverequestCommandHandler.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.Features.LeaveRequest.Request.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveRequest.Handler.Commands
 {
-    internal class UpdateLeaverequestCommandHandler
+    public class UpdateLeaveRequestCommandHandler : IRequestHandler<UpdateLeaveRequestCommand, Unit>
     {
+        private readonly ILeaveRequestRepository _leaveRequestRepository;
+        private readonly Mapper _mapper;
+
+        public UpdateLeaveRequestCommandHandler(ILeaveRequestRepository leaveRequestRepository, Mapper mapper)
+        {
+            _leaveRequestRepository = leaveRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveRequestCommand request, CancellationToken cancellationToken)
+        {
+            var leaverequest = await _leaveRequestRepository.Get(request.Id);
+            if (request.LeaveRequestDto != null)
+            {
+                _mapper.Map(request.LeaveRequestDto, leaverequest);
+                await _leaveRequestRepository.Update(leaverequest);
+            }
+            else if (request.ChangeLeaveRequestApprovalDto != null) {
+                await _leaveRequestRepository.ChangeApprovalStatus(leaverequest, request.ChangeLeaveRequestApprovalDto.Approved);
+            }
+            return Unit.Value;
+
+        }
     }
 }
