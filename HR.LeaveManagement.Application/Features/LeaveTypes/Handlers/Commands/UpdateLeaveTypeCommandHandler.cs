7c9f3403b00057a671fb2741a015063a0a5836ba
Nimesh8848 <nimeshmathema@gmail.com>
2024-11-29 10:44:46 +0545

    Update leavetype  done

--- HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/UpdateLeaveTypeCommandHandler.cs
+++ HR.LeaveManagement.Application/Features/LeaveTypes/Handlers/Commands/UpdateLeaveTypeCommandHandler.cs
@@ -1,14 +1,32 @@
-﻿using HR.LeaveManagement.Application.DTOs.LeaveType;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs.LeaveType;
+using HR.LeaveManagement.Application.Features.LeaveTypes.Request.Commands;
+using HR.LeaveManagement.Application.Persistence.Contracts;
 using MediatR;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace HR.LeaveManagement.Application.Features.LeaveTypes.Handlers.Commands
 {
-    public class UpdateLeaveTypeCommand : IRequest<Unit>
+    public class UpdateLeaveTypeCommandHandler : IRequestHandler<UpdateLeaveTypeCommand,Unit>
     {
-        public LeaveTypeDto LeaveTypeDto { get; set; }
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
 
+        public UpdateLeaveTypeCommandHandler(ILeaveTypeRepository leaveTypeRepository, IMapper mapper) {
+            _leaveTypeRepository = leaveTypeRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(UpdateLeaveTypeCommand request, CancellationToken cancellationToken)
+        {
+            var leavetype =await _leaveTypeRepository.Get(request.LeaveTypeDto.Id);
+            _mapper.Map(request.LeaveTypeDto, leavetype);
+            await _leaveTypeRepository.Update(leavetype);
+            return Unit.Value;
+        }
     }
 }
